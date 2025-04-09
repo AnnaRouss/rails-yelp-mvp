1acdc35d385602bf773bf3116240e44465ecb6e1
AnastasiaRoussakis <a_roussakis@outlook.com>
2025-04-08 20:11:48 -0400

    rails new

--- app/models/restaurant.rb
+++
@@ -1,8 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
-
-  validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
-end
